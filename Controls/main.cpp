fec135ff812013ca74fba63406525f524506b872
ExecTio9 <gbc0005@uah.edu>
2023-11-09 12:32:56 -0600

    Added basics

--- Controls/main.cpp
+++ Controls/main.cpp
@@ -2,3 +2,13 @@
 // SPAROW CONTROLS MAIN
 // DESC. - Main branch of controls systems 
 // ****************************************
+
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+int main()
+{
+    /* code */
+    return 0;
+}
--- Stability/main.cpp
+++ Stability/main.cpp
@@ -2,3 +2,13 @@
 // SPAROW STABILITY MAIN
 // DESC. - Main branch of stability systems 
 // ****************************************
+
+#include <string.h>
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+    /* code */
+    return 0;
+}
