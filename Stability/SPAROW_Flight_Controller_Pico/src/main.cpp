a1f5ded12ef5aaac9221c9090c15ee3bb269fbd7
KV0042 <113254904+KV0042@users.noreply.github.com>
2024-02-22 17:05:16 -0600

    Libraires Added

--- Stability/SPAROW_Flight_Controller_Pico/src/main.cpp
+++ Stability/SPAROW_Flight_Controller_Pico/src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <Servo.h>
+#include <PID_v1.h>
+#include <EEPROM.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  
 }
