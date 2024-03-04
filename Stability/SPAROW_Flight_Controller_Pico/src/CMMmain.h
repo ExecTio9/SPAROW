9467fe2d6efc91d14934f4da7aba398970f854b6
Charles-Marmann <113216303+Charles-Marmann@users.noreply.github.com>
2024-03-03 19:46:17 -0600

    Added roll, still need to do servo control processing
    Also fixed CMMmain, you no longer need to comment it out.

--- Stability/SPAROW_Flight_Controller_Pico/src/CMMmain.h
+++ Stability/SPAROW_Flight_Controller_Pico/src/CMMmain.h
@@ -11,38 +11,50 @@
 
 unsigned long millisOld;
 float pitchM, rollM, pitch, roll, dt;
-const int initialServoValue = 100;
 
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
-PWMServo servo;
+
 
 // Initialize PID algorithm
-double desired = 0;
-double input, outputPID;
-double kp = 0.5, ki = 0.5, kd = 0.5;
-PID pid(&input, &outputPID, &desired, kp, ki, kd, DIRECT);
+double desired_Pitch = 0;
+double input_Pitch;
+double outputPID_Pitch;
+double kp_Pitch = 0.5, ki_Pitch = 0.5, kd_Pitch = 0.5;
+PID pidP(&input_Pitch, &outputPID_Pitch, &desired_Pitch, kp_Pitch, ki_Pitch, kd_Pitch, DIRECT);
 
-const int initialServoValue = 90;
+double desired_Roll = 0;
+double input_Roll;
+double outputPID_Roll;
+double kp_Roll = 0.5, ki_Roll = 0.5, kd_Roll = 0.5;
+PID pidR(&input_Roll, &outputPID_Roll, &desired_Roll, kp_Roll, ki_Roll, kd_Roll, DIRECT);
+
+const int initialServoPitchValue = 90;
+const int initialServoRollValue = 0;
 
 int lastdata;
 int lastVcal = 0;
-int lastPID_O = 0;
-float degree1 = 0;
-float degree2;
-float rVelocityZ;
-float PID_O_1 = 0;
-float PID_O_R = 0;
+int lastDegCal = 0;
+float degree_Pitch_1 = 0;
+float degree_Pitch_2;
+float degree_Roll_1 = 0;
+float degree_Roll_2;
+float rVelocity_Pitch;
+float rVelocity_Roll;
 PWMServo pitchServo;
+PWMServo rollServo;
 
 void setup()
 {
+
   pitchServo.attach(0);
+  rollServo.attach(1);
   Serial.begin(115200);
   int eeAddress = 0;
   long bnoID;
   bool foundCalib = false;
+
   if(bno.begin())
   {
     EEPROM.get(eeAddress, bnoID);
@@ -78,65 +90,59 @@ void setup()
     
   }
   millisOld = millis();
-  pitchServo.write(initialServoValue);
+  pitchServo.write(initialServoPitchValue);
+  rollServo.write(initialServoRollValue);
 
-    servo.attach(0);
-    servo.write(initialServoValue);
-    pid.SetMode(AUTOMATIC);
-
-  
-  // Get current degrees for Z axis
+    
+  // Get current degrees for P axis
   
 }
 
 void loop()
 {
+  if((millis() - lastdata)> 100){
   imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-  pitchM = atan2(acc.x()/9.8, acc.z()/9.8)/2/3.141592654*360;
+  pitchM = atan2(acc.x()/9.8, acc.z()/9.8)/2/3.141592654*360; // Pitch based off acceleration
   rollM = atan2(acc.y()/9.8, acc.z()/9.8)/2/3.141592654*360;
 
   dt = (millis()-millisOld)/1000.0;
   millisOld = millis();
   
-  pitch = (pitch-gyro.y()*dt)*0.98 + pitchM*0.02;
+  pitch = (pitch-gyro.y()*dt)*0.98 + pitchM*0.02; 
   roll = (roll+gyro.x()*dt)*0.98 + rollM*0.02;
 
+  lastdata = millis();
+  }
+
+  
+
   if((millis() - lastVcal)> 100){               // Calculate Rotational Velocity on all axis
-    rVelocityZ = (degree2-degree1)/0.1;
-    degree1 = degree2;
+    rVelocity_Pitch = (degree_Pitch_2 - degree_Pitch_1)/0.1;
+    degree_Pitch_1 = degree_Pitch_2;
+    rVelocity_Roll = (degree_Roll_2 - degree_Roll_1)/0.1;
+    degree_Roll_1 = degree_Roll_2;
     lastVcal = millis();
   }
 
-  // if((millis() - lastPID_O)> 100){
-  //   PID_O_R = (outputPID-PID_O_1)/0.1;
-  //   PID_O_1 = outputPID;
-  //   lastPID_O = millis();
-  // }
-
-  // Apply correction value for Z axis
-  int ServoInputZ;
-  ServoInputZ = PID_O_R;
-  //Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  
+  
   if((millis() - lastdata)> 100){
-  printf("rVelocityZ: ,%f,   PIDrate: ,%f,\n",rVelocityZ,PID_O_R);
-  /*
-  Serial.print("rVelocityZ: ");
-  Serial.println(rVelocityZ);
-  Serial.print("PIDrate: ");
-  Serial.println(PID_O_R);
-  */
-  //Serial.print("newZ: ");
-  //Serial.println(newZ);
+  printf("rVelocityP: ,%f,   PIDoutput: ,%f,\n",rVelocity_Pitch,outputPID_Pitch);
   lastdata = millis();
   }
+
   // Limit servo to min 0, or max 180 degrees
-  if (ServoInputZ < -90) {
-    ServoInputZ = -90;
-  } else if (ServoInputZ > 90) {
-    ServoInputZ = 90;
+  if (outputPID_Pitch < -90) {
+    outputPID_Pitch = -90;
+  } else if (outputPID_Pitch > 90) {
+    outputPID_Pitch = 90;
   }
-  //servo.write(newZ + initialServoValue); // Add initialServoValue to make sure Servo is between 0 and 180
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+  if (outputPID_Roll < -90) {
+    outputPID_Roll = -90;
+  } else if (outputPID_Roll > 90) {
+    outputPID_Roll = 90;
+  }
+  //servo.write(ServoInputP); // Add initialServoValue to make sure Servo is between 0 and 180
 }
